d1149a0ddb8fb98b5edef507b5907997a0643964
graceelliott8374-tech <graceelliott8374@gmail.com>
2026-02-04 12:36:30 -0500

    chore: run seedAdmins on startup temporarily

--- server/src/index.js
+++ server/src/index.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 5000;
 async function start() {
   try {
     await connectDB();
+    // TEMP: seed admins on startup (remove after first successful deploy)
+    require("./scripts/seedAdmins");
 
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
